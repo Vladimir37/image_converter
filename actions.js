53cc027039765e96457a8ed691f690387171e385
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-26 03:30:14 +0000

    0.5

--- actions.js
+++ actions.js
@@ -3,19 +3,99 @@ var ffi = require('ffi');
 var ref = require('ref');
 var formidable = require('formidable');
 var mime = require('mime-types');
+var Struct = require('ref-struct');
+var is = require('image-size');
+var hexy = require('hexy');
 
-//var RTLD_NOW = ffi.DynamicLibrary.FLAGS.RTLD_NOW;
-//var RTLD_GLOBAL = ffi.DynamicLibrary.FLAGS.RTLD_GLOBAL;
-//var mode = RTLD_NOW | RTLD_GLOBAL;
-
-var my_rand = ffi.Library('./libcerno', {
-    'my_rand': ['double', []]
+var ffiImage = Struct({
+    'rows': 'int',
+    'cols': 'int',
+    'data': 'int'
 });
 
-console.log(my_rand.my_rand());
+var ffi_image = ref.refType(ffiImage);
+
+var libcerno = ffi.Library('./libcerno', {
+    'my_rand': ['double', []],
+    'compare_images': ['double', ['int', ffi_image, ffi_image, ffi_image, ffi_image]]
+});
+
+//test ------------------------------------------------------------------
+var all = {
+    img1: null,
+    img2: null,
+    resolution1: null,
+    resolution2: null
+};
+var im_r1 = new Promise(function(resolve, reject) {
+    fs.readFile('images/1453770075207.jpeg', function(err, result) {
+        if(err) {
+            reject(err);
+        }
+        else {
+            all.img1 = result;
+            resolve();
+        }
+    });
+});
+var im_r2 = new Promise(function(resolve, reject) {
+    fs.readFile('images/1453775300708.jpeg', function(err, result) {
+        if(err) {
+            reject(err);
+        }
+        else {
+            all.img2 = result;
+            resolve();
+        }
+    });
+});
+var im_d1 = new Promise(function(resolve, reject) {
+    is('images/1453770075207.jpeg', function(err, result) {
+        if(err) {
+            reject(err);
+        }
+        else {
+            all.resolution1 = result;
+            resolve();
+        }
+    });
+});
+var im_d2 = new Promise(function(resolve, reject) {
+    is('images/1453775300708.jpeg', function(err, result) {
+        if(err) {
+            reject(err);
+        }
+        else {
+            all.resolution2 = result;
+            resolve();
+        }
+    });
+});
+
+Promise.all([im_r1, im_r2, im_d1, im_d2]).then(function(){
+    var img_s_1 = new ffiImage({
+        'rows': all.resolution1.height,
+        'cols': all.resolution1.width,
+        'data': all.img1
+    });
+    var img_s_2 = new ffiImage({
+        'rows': all.resolution2.height,
+        'cols': all.resolution2.width,
+        'data': all.img2
+    });
+    var res1 = new ffiImage();
+    var res2 = new ffiImage();
+    console.log(hexy(all.img2));
+    //var result = libcerno.compare_images(5, img_s_1.ref(), img_s_2.ref(), res1.ref(), res2.ref());
+    console.log('END');
+    console.log(result);
+}, function(err) {
+    console.log('ERROR');
+});
+//test ------------------------------------------------------------------
 
 function index(req, res, next) {
-    console.log(my_rand());
+    console.log(libcerno.my_rand());
     res.render('index.html');
 };
 
@@ -38,13 +118,21 @@ function upload(req, res, next) {
         }
         var img = files.one;
         var ext = mime.extension(img.type);
-        fs.rename(img.path, 'images/' + new Date().getTime() + '.' + ext, function(err) {
-            if (err) {
-                res.end('Error!');
+        is(img.path, function(err, data) {
+            if(err) {
+                console.log(err);
             }
             else {
-                res.end('Success!');
+                console.log(data);
             }
+            fs.rename(img.path, 'images/' + new Date().getTime() + '.' + ext, function (err) {
+                if (err) {
+                    res.end('Error!');
+                }
+                else {
+                    res.end('Success!');
+                }
+            });
         });
     });
 };
