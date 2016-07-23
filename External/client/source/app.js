1eeaee941f8e6ea6fce7a74d2ac30c54d826281e
Vladimir37 <vladimir_zapas@mail.ru>
2016-07-23 23:58:28 +0000

    GUI 0.1

--- External/client/source/app.js
+++ External/client/source/app.js
@@ -37,3 +37,83 @@ app.controller('login', function ($scope, $http) {
         });
     };
 });
+
+app.controller('manage', function($scope, $http) {
+    $scope.user_data = {
+        type: '1',
+        status: '0'
+    };
+
+    $scope.create_user = function() {
+        if (!$scope.user_data.name || !$scope.user_data.pass) {
+            $scope.error = 'Required fields are empty';
+            return false;
+        }
+        $http({
+            method: 'POST',
+            url: '/manage',
+            data: $scope.user_data
+        }).then(function () {
+            window.location.reload();
+        });
+    };
+    $scope.delete_user = function (id) {
+        var deleted_data = {
+            type: '2',
+            id
+        };
+        $http({
+            method: 'POST',
+            url: '/manage',
+            data: deleted_data
+        }).then(function () {
+            window.location.reload();
+        });
+    }
+});
+app.controller('index', function($scope, $http) {
+    $scope.value_upload = 'Upload image';
+    setInterval(function () {
+        var value = angular.element('#upload_but').val();
+        $scope.$apply(function () {
+            $scope.value_upload = value ? 'Uploaded' : 'Upload image';
+        });
+    }, 500);
+
+    $scope.value_all = 'Upload image';
+    setInterval(function () {
+        var value = angular.element('#all_but').val();
+        $scope.$apply(function () {
+            $scope.value_all = value ? 'Uploaded' : 'Upload image';
+        });
+    }, 500);
+
+    $scope.value_compare_one = 'Upload image';
+    setInterval(function () {
+        var value = angular.element('#one_but').val();
+        $scope.$apply(function () {
+            $scope.value_compare_one = value ? 'Uploaded' : 'Upload image';
+        });
+    }, 500);
+
+    $scope.value_compare_two = 'Upload image';
+    setInterval(function () {
+        var value = angular.element('#two_but').val();
+        $scope.$apply(function () {
+            $scope.value_compare_two = value ? 'Uploaded' : 'Upload image';
+        });
+    }, 500);
+
+    $scope.upload_image = function () {
+        angular.element('#upload_but').click();
+    };
+    $scope.upload_all = function () {
+        angular.element('#all_but').click();
+    };
+    $scope.upload_compare_one = function () {
+        angular.element('#one_but').click();
+    };
+    $scope.upload_compare_two = function () {
+        angular.element('#two_but').click();
+    };
+});
