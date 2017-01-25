2323b4630dbc44c22ea3b14e7bba10b1ea260bf9
Vladimir37 <vladimir37work@gmail.com>
2017-01-25 18:52:49 +0300

    2.1.6

--- External/client/source/app.js
+++ External/client/source/app.js
@@ -265,9 +265,9 @@ app.controller('result_many', function($scope, $http) {
         });
 
         Promise.all(imgs_res).then(function (data) {
-            $('#first-card-ref').html(_generateData(data[0], 'one'));
-            $('#second-card-ref').html(_generateData(data[1], 'two'));
-            $('#third-card-ref').html(_generateData(data[2], 'three'));
+            $('#first-card').html(_generateData(data[0], 'one'));
+            $('#second-card').html(_generateData(data[1], 'two'));
+            $('#third-card').html(_generateData(data[2], 'three'));
 
             var D = calculateD($scope.data.one.number, $scope.data.two.number, $scope.data.three.number);
 
@@ -329,7 +329,7 @@ app.controller('result_many', function($scope, $http) {
                 break;
             }
 
-            var sausage_first = $scope.data[numbers[canv_num]].second.data;
+            var sausage_first = $scope.data[numbers[canv_num]].first.data;
             var width_first = canvas_first.width;
             var height_first = canvas_first.height;
 
@@ -346,7 +346,7 @@ app.controller('result_many', function($scope, $http) {
                 }
             }
             // ----
-            var sausage_second = $scope.data[numbers[canv_num]].first.data;
+            var sausage_second = $scope.data[numbers[canv_num]].second.data;
             var width_second = canvas_second.width;
             var height_second = canvas_second.height;
 
